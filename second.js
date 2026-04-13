10bf7c6be9bdf023c8aaf78443dab5405f852685
Protyay Kolay <protyaykolay10@gmail.com>
2026-04-13 09:58:23 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,11 @@
+// (function(exports, require, module, __filename, __dirname){
+
+protyay = {
+    name: "Protyay",
+    favNum: 36,
+    developer: true
+}
+// console.log(exports, require, module, __filename, __dirname)
+module.exports = protyay;
+    
+// })
